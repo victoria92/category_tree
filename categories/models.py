a5f22c9dd27bf06ed99c596c29756faf6b4af148
Victoria Hristova <viki.hristova@gmail.com>
2024-02-29 15:54:24 +0200

    Add parent category

--- categories/models.py
+++ categories/models.py
@@ -4,4 +4,8 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    image = models.ImageField(upload_to="images/", null=True, blank=True)
+    parent = models.ForeignKey("self", blank=True, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} id {}".format(self.name, self.id)
--- categories/views.py
+++ categories/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 
 from categories.models import Category
 from categories.serializers import CategorySerializer
@@ -33,6 +34,14 @@ class CategoryDetail(
 
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        request.data["parent"] = kwargs["pk"]
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
